1807524e81e28afc14ab1838fba79c4778e91ecf
desaxes <desaxes24@gmail.com>
2023-12-15 17:51:49 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { GameQueryModel } from './models/GameQueryModel'
 import { GameViewModel } from './models/GameViewModel'
 import { URIParamsModel } from './models/URIParamsModel'
 export const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 
 const jsonBodyMiddleware = express.json()
 app.use(jsonBodyMiddleware)
